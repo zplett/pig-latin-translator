da2e911424ccc513eaa949dbcff64272727ceeeb
Zac Plett <zplett@clyde>
2017-10-03 17:56:57 -0400

    added comments to pig.c, unpig.c, pigify.c, created unpig and pig man pages

--- pig.c
+++ pig.c
@@ -4,13 +4,19 @@
 #include "pigify.h"
 #include "pig.h"
 
-/* Static function definitions */
-/* External function definitions */
-
-/** This is the main function in pigify()
- *
- *
+/** This program reads input flags if any are entered and if not calls the translation function 
+ * to translate from English to Pig Latin.
  */
 int main(int argc, char *argv[]){
+  if (argc > 1) {
+    if ( ((strcmp(argv[1], "-?")) == 0) || ((strcmp(argv[1], "-h")) == 0) ) {
+	printf("Welcome to pig! Pig is a c program that converts words in English to Pig Latin. To use it just run the program and start typing!\n");
+	return 0;
+      }
+    else {
+      printf("Unknown flag! Please rerun the program.\n");
+      return 0;
+    }
+  }
   structure(&pigify);
 }
