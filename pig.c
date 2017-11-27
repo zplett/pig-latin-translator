7ac71f758411eebea768d88bc4ffa0f285bee03d
Zac Plett <zplett@clyde>
2017-11-27 14:44:41 -0500

    commit to new repo

--- pig.c
+++ pig.c
@@ -1,22 +1,48 @@
 #include <stdio.h>
-#include <string.h>
-#include <ctype.h>
-#include "pigify.h"
 
-/** This program reads input flags if any are entered and if not calls the translation function 
- * to translate from English to Pig Latin.
- */
+#define BUFFER_LENGTH 102
 
-int main(int argc, char *argv[]){
-  if (argc > 1) {
-    if ( ((strcmp(argv[1], "-?")) == 0) || ((strcmp(argv[1], "-h")) == 0) ) {
-	printf("Welcome to pig! Pig is a c program that converts words in English to Pig Latin. To use it just run the program and start typing!\n");
-	return 0;
+void pigify(static char (*)[]); 
+int is_vowel(char, int);
+
+int main() {
+  
+}
+
+void pigify(static char (*arr)[]) {
+  char local_arr[BUFFER_LENGTH], ay[2], yay[3];
+  int count = 0;
+  strcopy(ay, "ay");
+  strcopy(yay, "yay");
+  for (i = 0; i < BUFFER_LENGTH; i++) {
+    if ((*arr)[i] == '\0') break;
+    int valid = is_vowel((*arr)[i], i);
+    if (valid == 0) {
+      if (i == 0) {
+	strcat((*arr), yay);
+	break;
       }
+      else if (i > 1) {
+	for (int j = 0; j < BUFFER_LENGTH; j++) { if (local_arr[j] == '\0') break; else count++; }
+	for (int k = 0; k < BUFFER_LENGTH; k++) { 
+	  (*arr)[k] = (*arr)[k+count];
+	  (*arr)[k+count] = '\0';
+	}
+	strcat((*arr), local_arr);
+	strcat((*arr), ay);
+	break;
+      }
+    }
     else {
-      printf("Unknown flag! Please rerun the program.\n");
-      return 0;
+      local_arr[i] = (*arr)[i];
     }
   }
-  structure(&pigify);
+}
+
+int is_vowel(char ch, int count) {
+ ch = tolower(ch);
+ switch(ch):
+ case 'a': case 'e': case 'i': case 'o': case 'u': return 0;
+ case 'y': if (count > 0) return 0;
+ default: return 1;
 }
